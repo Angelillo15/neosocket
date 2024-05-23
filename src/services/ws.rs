e4025e2d2cd2b4079828c7827403ac33bd3a8e40
Angelillo15 <contact@angelillo15.es>
2024-05-23 23:24:15 +0200

    Implement actor and actix::Handler with messages

--- src/services/ws.rs
+++ src/services/ws.rs
@@ -1,18 +1,21 @@
-use std::sync::{Arc, Mutex};
 use std::sync::mpsc::channel;
-use actix::{Actor, Addr, Message, StreamHandler};
-use actix_web::{get, Error, HttpRequest, HttpResponse, post, web};
-use actix_web::web::Data;
-use actix_web_actors::ws;
-use log::debug;
-use serde::Deserialize;
-use uuid::Uuid;
-use crate::AppState;
-use crate::ws::channel_manager::ChannelManager;
+use std::sync::{Arc, Mutex};
 
-#[derive(Debug, Clone)]
-struct Handler {
-    channel_manager: Arc<Mutex<ChannelManager>>,
+use actix::{Actor, Addr, AsyncContext, Message, Recipient, StreamHandler};
+use actix_web::web::Data;
+use actix_web::{get, web, Error, HttpRequest, HttpResponse};
+use actix_web_actors::ws;
+use log::{debug, error};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+use crate::managers::channel_manager::ChannelManager;
+use crate::managers::{recipient_manager, ConnectedMessage, PropagateMessage};
+use crate::{AppState, RecipientManager};
+
+#[derive(Clone)]
+pub struct Handler {
+    pub app_state: Data<AppState>,
     uuid: String,
     client_id: Uuid,
 }
@@ -24,6 +27,28 @@ struct Info {
 
 impl Actor for Handler {
     type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        let addr = ctx.address().clone();
+        let recipient = addr.clone().recipient();
+
+        {
+            let connected_recipient: Recipient<ConnectedMessage> = addr.clone().recipient();
+
+            connected_recipient.do_send(ConnectedMessage {
+                channel_uuid: self.uuid.clone().parse().expect("Could not parse uuid"),
+                connection_id: self.client_id.to_string(),
+            });
+        }
+
+        let app_state = self.app_state.clone();
+
+        {
+            let mut recipient_manager = app_state.recipient_manager.lock().unwrap();
+            recipient_manager.add_recipient(self.client_id.to_string(), recipient);
+            debug!("Added recipient: {:?}", self.client_id);
+        }
+    }
 }
 
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Handler {
@@ -32,12 +57,58 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Handler {
             Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
             Ok(ws::Message::Pong(_)) => (),
             Ok(ws::Message::Text(text)) => {
+                let app_state = self.app_state.clone();
+                let recipient_manager = app_state.recipient_manager.clone();
+
+                {
+                    let recipient_manager = recipient_manager.lock().unwrap();
+                    recipient_manager.propagate_message(
+                        self.client_id.to_string(),
+                        text.clone().parse().expect("Could not parse message"),
+                    );
+                }
+
                 debug!("Text: {}", text);
-            },
+            }
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            Ok(ws::Message::Close(reason)) => ctx.close(reason),
+            Ok(ws::Message::Close(reason)) => {
+                let app_state = self.app_state.clone();
+
+                {
+                    let mut recipient_manager = app_state.recipient_manager.lock().unwrap();
+                    recipient_manager.remove_recipient(self.client_id.to_string());
+
+                    if recipient_manager.recipients.len() == 0 {
+                        let channel_manager = app_state.channel_manager.clone();
+                        let mut channel_manager = channel_manager.lock().unwrap();
+                        let uuid = Uuid::parse_str(&self.uuid).unwrap();
+                        channel_manager.mark_channel_as_inactive(uuid);
+                    }
+
+                    debug!("Removed recipient: {:?}", self.client_id);
+                }
+
+                debug!("Closing connection: {:?}", self.client_id);
+                ctx.close(reason);
+            }
             _ => (),
         }
+    }
+}
+
+impl actix::Handler<PropagateMessage> for Handler {
+    type Result = ();
+
+    fn handle(&mut self, msg: PropagateMessage, ctx: &mut Self::Context) {
+        ctx.text(serde_json::to_string(&msg).expect("Could not serialize message"));
+    }
+}
+
+impl actix::Handler<ConnectedMessage> for Handler {
+    type Result = ();
+
+    fn handle(&mut self, msg: ConnectedMessage, ctx: &mut Self::Context) {
+        ctx.text(serde_json::to_string(&msg).expect("Could not serialize message"));
     }
 }
 
@@ -46,20 +117,38 @@ async fn index(
     info: web::Path<Info>,
     app_state: Data<AppState>,
     req: HttpRequest,
-    stream: web::Payload
+    stream: web::Payload,
 ) -> Result<HttpResponse, Error> {
     let id = info.uuid.clone();
     let channel_manager = app_state.channel_manager.clone();
 
-    if !channel_manager.lock().unwrap().channel_exists(id.clone()) {
-        return Ok(HttpResponse::NotFound().finish());
+    {
+        let mut channel_manager = channel_manager.lock().unwrap();
+
+        if !channel_manager.channel_exists(id.clone()) {
+            return Ok(HttpResponse::NotFound().finish());
+        }
+
+        match Uuid::parse_str(&id) {
+            Ok(uuid) => {
+                channel_manager.mark_channel_as_active(uuid);
+            }
+            Err(_) => {
+                error!("Could not parse uuid: {}", id.clone());
+                return Ok(HttpResponse::NotFound().finish());
+            }
+        }
     }
 
-    let resp = ws::start(Handler {
-        channel_manager: channel_manager.clone(),
-        uuid: id,
-        client_id: Uuid::new_v4(),
-    }, &req, stream);
+    let resp = ws::start(
+        Handler {
+            app_state: app_state.clone(),
+            uuid: id,
+            client_id: Uuid::new_v4(),
+        },
+        &req,
+        stream,
+    );
 
     debug!("{:?}", resp);
     resp
