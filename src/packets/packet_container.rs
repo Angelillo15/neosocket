eafa97d7091e27701c201205f602e0fce5c82bfd
Angelillo15 <contact@angelillo15.es>
2024-06-01 14:13:12 +0200

    Create packet_container.rs

--- src/packets/packet_container.rs
+++ src/packets/packet_container.rs
@@ -5,5 +5,25 @@ use serde::{Deserialize, Serialize};
 #[rtype(result = "()")]
 pub struct PacketContainer<T> {
     pub packet_id: u8,
+    #[serde(skip_serializing_if = "String::is_empty")]
+    pub packet_sender: String,
     pub packet: T,
+}
+
+impl <T> PacketContainer<T> {
+    pub fn new(packet_id: u8, packet_sender: String, packet: T) -> PacketContainer<T> {
+        PacketContainer {
+            packet_id,
+            packet_sender,
+            packet,
+        }
+    }
+
+    pub fn new_without_sender(packet_id: u8, packet: T) -> PacketContainer<T> {
+        PacketContainer {
+            packet_id,
+            packet_sender: String::new(),
+            packet,
+        }
+    }
 }
