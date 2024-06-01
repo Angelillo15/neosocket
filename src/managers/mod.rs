04f82b940e4845d46cc052fb267771697fe4ed03
Angelillo15 <contact@angelillo15.es>
2024-06-01 14:13:45 +0200

    Separate packets in their own file

--- src/managers/mod.rs
+++ src/managers/mod.rs
@@ -1,29 +1,3 @@
-use actix::{Message};
-use serde::{Deserialize, Serialize};
-
 pub mod channel;
 pub mod channel_manager;
 pub mod recipient_manager;
-
-#[derive(Message, Serialize, Deserialize, Clone)]
-#[rtype(result = "()")]
-pub struct PropagateMessage {
-    pub sender: String,
-    pub message: String,
-}
-
-#[derive(Message, Serialize, Deserialize, Clone)]
-#[rtype(result = "()")]
-pub struct ConnectedMessage {
-    pub channel_uuid: String,
-    pub connection_id: String,
-}
-
-impl PropagateMessage {
-    pub fn new(sender: String, message: String) -> PropagateMessage {
-        PropagateMessage {
-            sender,
-            message,
-        }
-    }
-}
